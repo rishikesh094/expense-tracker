49c012f478daafd034fcf56b4d1be0c469a36eb6
Rishikesh Singh <149285390+rishikesh094@users.noreply.github.com>
2025-07-05 11:59:57 +0530

    Update server.js cors policy update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://expense-tracker-pz8m.onrender.com",
     credentials: true,
   })
 );
