585bfc2fa338d78870cbe42f0424bc6661f1c4ce
Rishikesh Singh <149285390+rishikesh094@users.noreply.github.com>
2025-07-05 11:45:14 +0530

    Update auth.js backend new update v1

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -56,6 +56,8 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
+      secure:true,
+      sameSite:'None',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     res.json({
